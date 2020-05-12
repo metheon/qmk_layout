04490a50d89494427bb30d56a3d8d30d326a7d41
Kenneth Ehrenreich-Zak <kenneth@ehrenreich-zak.dk>
2020-05-12 10:14:14 +0200

    Extract general shortcut defines into own file

---
+++ users/metheon/shortcuts.h
@@ -0,0 +1,24 @@
+#define UNDO                LCTL(KC_Z)
+#define REDO                LSFT(LCTL(KC_Z))
+#define CUT                 LCTL(KC_X)
+#define COPY                LCTL(KC_C)
+#define PASTE               LCTL(KC_V)
+
+#define SPACES              LGUI(KC_TAB)            // I think it's called Desktops in Windows actually
+#define LEFT_MON            LGUI(LSFT(KC_LEFT))     // Move window to left monitor
+#define RGHT_MON            LGUI(LSFT(KC_RGHT))     // Move window to right monitor
+#define LEFT_SPC            LGUI(LCTL(KC_LEFT))     // Move to left space
+#define RGHT_SPC            LGUI(LCTL(KC_RGHT))     // Move to right space
+#define WIN_MENU            LCTL(KC_ESC)            // Open the win menu
+#define DESKTOP             LGUI(KC_D)              // Expose the Desktop
+#define LFT_ALGN            LGUI(KC_LEFT)           // Align the window to the left half of the screen
+#define RGT_ALGN            LGUI(KC_RGHT)           // Align the window to the right half of the screen
+#define MAXIMIZE            LGUI(KC_UP)             // Maximize the window
+
+#define VID_OFF             LCTL(LSFT(KC_O))        // Turns off camera in Teams
+#define MIC_OFF             LCTL(LSFT(KC_M))        // Turns off microphone in Teams
+
+#define LOCK_PC             LGUI(KC_L)              // Lock the PC
+#define DEL_WRD             LCTL(KC_BSPC)           // Backwards delete a whole word
+#define FILES               LGUI(KC_E)              // Open File Explorer
+#define ESC_MEH             MEH_T(KC_ESC)           // Escape on tap, meh on hold (I use that for virtual box)
