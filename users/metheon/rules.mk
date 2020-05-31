93c44eeff3ffdea88e45fc604011a9552e00ac32
Kenneth Ehrenreich-Zak <kenneth@ehrenreich-zak.dk>
2020-05-31 06:14:38 +0200

    Reduce keymap size

--- users/metheon/rules.mk
+++ users/metheon/rules.mk
@@ -1,12 +1,12 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-LINK_TIME_OPTIMIZATION_ENABLE	=	no	# Reduces compiled size of firmware
+LINK_TIME_OPTIMIZATION_ENABLE	=	yes # Reduces compiled size of firmware
 COMMAND_ENABLE					=	no 	# Commands for debug and configuration
 AUDIO_ENABLE					=	no	# Enables any onboard speakers
 MOUSEKEY_ENABLE					=	yes # Enables usage of keyboard as mouse
 
 ifndef ENCODER_ENABLE
-	ENCODER_ENABLE					=	no  # Enables usage of rotary encoders
+	ENCODER_ENABLE				=	no  # Enables usage of rotary encoders
 endif
 
 DYNAMIC_MACRO_ENABLE			=	yes # Enables dynamic macros
