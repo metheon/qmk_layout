4f058aeca4755e56a5a4a1f0798401b5c645b6a1
Kenneth Ehrenreich-Zak <kenneth@ehrenreich-zak.dk>
2020-06-04 13:21:40 +0200

    Enforce disabling of a bunch of features, to save a bit of space

--- users/metheon/rules.mk
+++ users/metheon/rules.mk
@@ -1,8 +1,6 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
 LINK_TIME_OPTIMIZATION_ENABLE	=	yes # Reduces compiled size of firmware
-COMMAND_ENABLE					=	no 	# Commands for debug and configuration
-AUDIO_ENABLE					=	no	# Enables any onboard speakers
 MOUSEKEY_ENABLE					=	yes # Enables usage of keyboard as mouse
 
 ifndef ENCODER_ENABLE
@@ -18,3 +16,16 @@ SRC += encoders.c
 SRC += tap_dance.c
 SRC += process_records.c
 SRC += leader.c
+
+# Disable as many features as possible
+COMMAND_ENABLE					=	no 	# Commands for debug and configuration
+AUDIO_ENABLE					=	no	# Enables any onboard speakers
+KEY_LOCK_ENABLE					=	no
+CONSOLE_ENABLE					=	no
+VELOCIKEY_ENABLE				=	no
+STENO_ENABLE					=	no
+BOOTMAGIC_ENABLE				=	no
+TERMINAL_ENABLE					=	no
+GRAVE_ESC_ENABLE				=	no
+MAGIC_ENABLE					=	no
+SPACE_CADET_ENABLE				=	no
