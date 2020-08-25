e5e57b092ad847b39773037a99a3b53d695965d9
Kenneth Ehrenreich-Zak <kenneth@ehrenreich-zak.dk>
2020-08-25 08:35:31 +0200

    Reshuffle encoder on-press functions

--- users/metheon/encoders.h
+++ users/metheon/encoders.h
@@ -1,13 +1,13 @@
 // Encoder tap functions
-#define BSE_E01             KC_ENTER
-#define BSE_E02             KC_NO
+#define BSE_E01             KC_NO
+#define BSE_E02             KC_MUTE
 #define LWR_E01             KC_NO
 #define LWR_E02             KC_NO
 #define RSE_E01             KC_NO
 #define RSE_E02             KC_NO
-#define ADJ_E01             KC_TRNS
-#define ADJ_E02             KC_TRNS
-#define EXT_E01             KC_MUTE
+#define ADJ_E01             KC_NO
+#define ADJ_E02             KC_NO
+#define EXT_E01             KC_NO
 #define EXT_E02             KC_MPLY
 #define FUN_E01             KC_NO
 #define FUN_E02             KC_NO
