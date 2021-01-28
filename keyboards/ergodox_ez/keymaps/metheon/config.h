e70361099b4b1e0180336d19022f9dbd334ddd90
Kenneth Ehrenreich-Zak <kenneth@ehrenreich-zak.dk>
2021-01-28 19:49:56 +0100

    Kyria based layout instead of aysu/minidox

--- keyboards/ergodox_ez/keymaps/metheon/config.h
+++ keyboards/ergodox_ez/keymaps/metheon/config.h
@@ -1,18 +1,20 @@
 // clang-format off
+#define _NONE_ KC_NO
+
 #define LAYOUT_metheon( \
-        L11  ,L12  ,L13  ,L14  ,L15  ,R11  ,R12  ,R13  ,R14  ,R15  , \
-        L21  ,L22  ,L23  ,L24  ,L25  ,R21  ,R22  ,R23  ,R24  ,R25  , \
-        L31  ,L32  ,L33  ,L34  ,L35  ,R31  ,R32  ,R33  ,R34  ,R35  , \
-                    E01  ,L41  ,L42  ,R41  ,R42  ,E02                \
+        L00   ,L01   ,L02   ,L03   ,L04   ,L05                                    ,R05   ,R04   ,R03   ,R02   ,R01   ,R00   , \
+        L10   ,L11   ,L12   ,L13   ,L14   ,L15                                    ,R15   ,R14   ,R13   ,R12   ,R11   ,R10   , \
+        L20   ,L21   ,L22   ,L23   ,L24   ,L25   ,L26   ,L27        ,R27   ,R26   ,R25   ,R24   ,R23   ,R22   ,R21   ,R20   , \
+                             L30   ,L31   ,L32   ,L33   ,L34        ,R34   ,R33   ,R32   ,R31   ,R30                          \
     ) \
     LAYOUT_ergodox_pretty( \
-    KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,   KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,KC_NO,  \
-    KC_NO,L11,  L12,  L13,  L14,  L15,  KC_NO,   KC_NO,R11,  R12,  R13,  R14,  R15,  KC_NO,  \
-    KC_NO,L21,  L22,  L23,  L24,  L25,                 R21,  R22,  R23,  R24,  R25,  KC_NO,  \
-    KC_NO,L31,  L32,  L33,  L34,  L35,  KC_NO,   KC_NO,R31,  R32,  R33,  R34,  R35,  KC_NO,  \
-    KC_NO,KC_NO,KC_NO,KC_NO,E01,                             E02,  KC_NO,KC_NO,KC_NO,KC_NO,  \
-                                  KC_NO,KC_NO,   KC_NO,KC_NO,                                \
-                                        KC_NO,   KC_NO,                                      \
-                            L41,  L42,  KC_NO,   KC_NO,R41,  R42                             \
+        _NONE_,_NONE_,_NONE_,_NONE_,_NONE_,_NONE_,_NONE_                   ,_NONE_,_NONE_,_NONE_,_NONE_,_NONE_,_NONE_,_NONE_, \
+        L00   ,L01   ,L02   ,L03   ,L04   ,L05   ,L27                      ,R27   ,R05   ,R04   ,R03   ,R02   ,R01   ,R00   , \
+        L10   ,L11   ,L12   ,L13   ,L14   ,L15                                    ,R15   ,R14   ,R13   ,R12   ,R11   ,R10   , \
+        L20   ,L21   ,L22   ,L23   ,L24   ,L25   ,L26                      ,R26   ,R25   ,R24   ,R23   ,R22   ,R21   ,R20   , \
+        _NONE_,_NONE_,L30   ,L31   ,L32                                                  ,R32   ,R31   ,R30   ,_NONE_,_NONE_, \
+                                                  _NONE_,_NONE_     ,_NONE_,_NONE_,                                           \
+                                                         _NONE_     ,_NONE_,                                                  \
+                                           L33   ,L34   ,_NONE_     ,_NONE_,R34   ,R33                                        \
 )
 // clang-format on
