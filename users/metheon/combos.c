c65fcc803948ebdd7022736a7ee38e94f7a24956
Kenneth Ehrenreich-Zak <kenneth@ehrenreich-zak.dk>
2021-01-29 19:59:46 +0100

    Change combos

--- users/metheon/combos.c
+++ users/metheon/combos.c
@@ -1,17 +1,17 @@
 #include QMK_KEYBOARD_H
 
 enum combos {
-  W_F_Q,
+  W_F_TAB,
   U_Y_QUOT,
-  D_H_ESC,
+  L_U_ESC,
 };
 
-const uint16_t PROGMEM w_f_q[] = { KC_W, KC_F, COMBO_END};
+const uint16_t PROGMEM w_f_tab[] = { KC_W, KC_F, COMBO_END};
 const uint16_t PROGMEM u_y_quot[] = { KC_U, KC_Y, COMBO_END};
-const uint16_t PROGMEM d_h_esc[] = { KC_D, KC_H, COMBO_END};
+const uint16_t PROGMEM l_u_esc[] = { KC_L, KC_U, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-  [W_F_Q] = COMBO(w_f_q, KC_Q),
+  [W_F_TAB] = COMBO(w_f_tab, KC_TAB),
   [U_Y_QUOT] = COMBO(u_y_quot, KC_QUOT),
-  [D_H_ESC] = COMBO(d_h_esc, KC_ESC),
+  [L_U_ESC] = COMBO(l_u_esc, KC_ESC),
 };
