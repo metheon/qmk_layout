ccab11598ea0f37b6fc237d14172b249de5b993b
Kenneth Ehrenreich-Zak <kenneth@ehrenreich-zak.dk>
2021-03-01 11:39:04 +0100

    Add Kyria layout back

---
+++ keyboards/kyria/keymaps/metheon/config.h
@@ -0,0 +1,42 @@
+#pragma once
+
+// The Kyria maps fine to a 5x3+3 layout, just a lot of unused keys
+
+// clang-format off
+#define LAYOUT_metheon( \
+        L00   ,L01   ,L02   ,L03   ,L04   ,R04   ,R03   ,R02   ,R01   ,R00   , \
+        L10   ,L11   ,L12   ,L13   ,L14   ,R14   ,R13   ,R12   ,R11   ,R10   , \
+        L20   ,L21   ,L22   ,L23   ,L24   ,R24   ,R23   ,R22   ,R21   ,R20   , \
+                      L30   ,L31   ,L32   ,R32   ,R31   ,R30                   \
+    ) \
+    LAYOUT( \
+        KC_NO ,L00   ,L01   ,L02   ,L03   ,L04                                    ,R04   ,R03   ,R02   ,R01   ,R00   ,KC_NO , \
+        KC_NO ,L10   ,L11   ,L12   ,L13   ,L14                                    ,R14   ,R13   ,R12   ,R11   ,R10   ,KC_NO , \
+        KC_NO ,L20   ,L21   ,L22   ,L23   ,L24   ,KC_NO ,KC_NO      ,KC_NO ,KC_NO ,R24   ,R23   ,R22   ,R21   ,R20   ,KC_NO , \
+                             KC_NO ,KC_NO ,L30   ,L31   ,L32        ,R32   ,R31   ,R30   ,KC_NO ,KC_NO                        \
+    )
+// clang-format on
+
+#ifdef RGBLIGHT_ENABLE
+  #define RGBLIGHT_ANIMATIONS
+  #define RGBLIGHT_HUE_STEP 8
+  #define RGBLIGHT_SAT_STEP 8
+  #define RGBLIGHT_VAL_STEP 8
+  #define RGBLIGHT_SLEEP
+  #define RGBLIGHT_SPLIT
+#endif
+
+// EC11K encoders have a different resolution than other EC11 encoders.
+// When using the default resolution of 4, if you notice your encoder skipping
+// every other tick, lower the resolution to 2.
+#define ENCODER_RESOLUTION 2
+
+// Allows to use either side as the master. Look at the documentation for info:
+// https://docs.qmk.fm/#/config_options?id=setting-handedness
+#define EE_HANDS
+
+// Allows media codes to properly register in macros and rotary encoder code
+#define TAP_CODE_DELAY 10
+
+// Reverse encoder directionality
+#define ENCODER_DIRECTION_FLIP
---
+++ keyboards/kyria/keymaps/metheon/rules.mk
@@ -0,0 +1,4 @@
+OLED_DRIVER_ENABLE		= no  # Enables the use of OLED displays
+ENCODER_ENABLE			= yes # Enables the use of one or more encoders
+RGBLIGHT_ENABLE			= yes # Enables underglow / backlight
+
