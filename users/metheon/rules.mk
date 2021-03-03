883594c8395bf1493ce75fc7209531db954762a3
Kenneth Ehrenreich-Zak <kenneth@ehrenreich-zak.dk>
2021-03-03 11:06:58 +0100

    Remove tap dance

--- users/metheon/rules.mk
+++ users/metheon/rules.mk
@@ -7,7 +7,6 @@ endif
 EXTRAKEY_ENABLE					=	yes 	# Enables audio control and System control
 MOUSEKEY_ENABLE					=	yes 	# Enables usage of keyboard as mouse
 LTO_ENABLE						=	yes		# Reduces compiled size of firmware
-TAP_DANCE_ENABLE				=	yes  	# Enables the tap dance feature
 COMBO_ENABLE					=	yes		# Enables the usage of combos
 
 SRC += metheon.c
@@ -15,10 +14,10 @@ SRC += encoders.c
 SRC += process_records.c
 SRC += which_os.c
 SRC += window_tab.c
-SRC += tap_dances.c
 SRC += combos.c
 
 # Disable as many features as possible
+TAP_DANCE_ENABLE				=	no	  	# Enables the tap dance feature
 LEADER_ENABLE					=	no  	# Enables the Leader Key feature
 BOOTMAGIC_ENABLE				=	no	    # Not required for CG_TOGG which is the only bootmagic feature I use
 DYNAMIC_MACRO_ENABLE			=	no 		# Enables dynamic macros
