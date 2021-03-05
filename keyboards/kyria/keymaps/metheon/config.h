5f9cb6cb02b98d9ab35db92b13f2fa0afda1a294
Kenneth Ehrenreich-Zak <kenneth@ehrenreich-zak.dk>
2021-03-05 11:18:42 +0100

    Move to Kyria based layout, again

--- keyboards/kyria/keymaps/metheon/config.h
+++ keyboards/kyria/keymaps/metheon/config.h
@@ -1,19 +1,19 @@
 #pragma once
 
-// The Kyria maps fine to a 5x3+3 layout, just a lot of unused keys
+// The Kyria 1:1 to a Kyria but we need the name change for the define
 
 // clang-format off
 #define LAYOUT_metheon( \
-        L00   ,L01   ,L02   ,L03   ,L04   ,R04   ,R03   ,R02   ,R01   ,R00   , \
-        L10   ,L11   ,L12   ,L13   ,L14   ,R14   ,R13   ,R12   ,R11   ,R10   , \
-        L20   ,L21   ,L22   ,L23   ,L24   ,R24   ,R23   ,R22   ,R21   ,R20   , \
-                      L30   ,L31   ,L32   ,R32   ,R31   ,R30                   \
+        L00   ,L01   ,L02   ,L03   ,L04   ,L05                                    ,R05   ,R04   ,R03   ,R02   ,R01   ,R00   , \
+        L10   ,L11   ,L12   ,L13   ,L14   ,L15                                    ,R15   ,R14   ,R13   ,R12   ,R11   ,R10   , \
+        L20   ,L21   ,L22   ,L23   ,L24   ,L25   ,L26   ,L27        ,R27   ,R26   ,R25   ,R24   ,R23   ,R22   ,R21   ,R20   , \
+                             L30   ,L31   ,L32   ,L33   ,L34        ,R34   ,R33   ,R32   ,R31   ,R30                          \
     ) \
     LAYOUT( \
-        KC_NO ,L00   ,L01   ,L02   ,L03   ,L04                                    ,R04   ,R03   ,R02   ,R01   ,R00   ,KC_NO , \
-        KC_NO ,L10   ,L11   ,L12   ,L13   ,L14                                    ,R14   ,R13   ,R12   ,R11   ,R10   ,KC_NO , \
-        KC_NO ,L20   ,L21   ,L22   ,L23   ,L24   ,KC_NO ,KC_NO      ,KC_NO ,KC_NO ,R24   ,R23   ,R22   ,R21   ,R20   ,KC_NO , \
-                             KC_NO ,KC_NO ,L30   ,L31   ,L32        ,R32   ,R31   ,R30   ,KC_NO ,KC_NO                        \
+        L00   ,L01   ,L02   ,L03   ,L04   ,L05                                    ,R05   ,R04   ,R03   ,R02   ,R01   ,R00   , \
+        L10   ,L11   ,L12   ,L13   ,L14   ,L15                                    ,R15   ,R14   ,R13   ,R12   ,R11   ,R10   , \
+        L20   ,L21   ,L22   ,L23   ,L24   ,L25   ,L26   ,L27        ,R27   ,R26   ,R25   ,R24   ,R23   ,R22   ,R21   ,R20   , \
+                             L30   ,L31   ,L32   ,L33   ,L34        ,R34   ,R33   ,R32   ,R31   ,R30                          \
     )
 // clang-format on
 
--- keyboards/planck/keymaps/metheon/config.h
+++ keyboards/planck/keymaps/metheon/config.h
@@ -1,21 +1,26 @@
 #pragma once
 
-#define _NONE_  KC_NO
-
-// A grid planck maps one to one to the Planck EZ, except for the 2U spacebar
+// A Planck EZ has 47 keys while the Kyria has 50. Two of the extra keys are encoders,
+// which I do not have on my Planck EZ. The two encoder spots are L30 and R30 and these
+// are simply left out. The remaining key is due to the Planck EZ having a 2U spacebar.
+// I compensate for the missing key simply by having that key function on a layer, still
+// clicked with the right thumb. It it not terribly convenient but it is okay for now,
+// until I get a grid Planck at some point. R33 left out as a result.
+// After this its simply a matter of aligning the Kyria thumb cluster along the bottom
+// row of the Planck EZ.
 
 // clang-format off
 #define LAYOUT_metheon( \
-        L00   ,L01   ,L02   ,L03   ,L04   ,R04   ,R03   ,R02   ,R01   ,R00   , \
-        L10   ,L11   ,L12   ,L13   ,L14   ,R14   ,R13   ,R12   ,R11   ,R10   , \
-        L20   ,L21   ,L22   ,L23   ,L24   ,R24   ,R23   ,R22   ,R21   ,R20   , \
-                      L30   ,L31   ,L32   ,R32   ,R31   ,R30                   \
+        L00   ,L01   ,L02   ,L03   ,L04   ,L05                                    ,R05   ,R04   ,R03   ,R02   ,R01   ,R00   , \
+        L10   ,L11   ,L12   ,L13   ,L14   ,L15                                    ,R15   ,R14   ,R13   ,R12   ,R11   ,R10   , \
+        L20   ,L21   ,L22   ,L23   ,L24   ,L25   ,L26   ,L27        ,R27   ,R26   ,R25   ,R24   ,R23   ,R22   ,R21   ,R20   , \
+                             L30   ,L31   ,L32   ,L33   ,L34        ,R34   ,R33   ,R32   ,R31   ,R30                          \
     ) \
     LAYOUT_planck_1x2uC( \
-        L00   ,L01   ,L02   ,L03   ,L04   ,_NONE_,_NONE_,R04   ,R03   ,R02   ,R01   ,R00   , \
-        L10   ,L11   ,L12   ,L13   ,L14   ,_NONE_,_NONE_,R14   ,R13   ,R12   ,R11   ,R10   , \
-        L20   ,L21   ,L22   ,L23   ,L24   ,_NONE_,_NONE_,R24   ,R23   ,R22   ,R21   ,R20   , \
-        _NONE_,_NONE_,L30   ,L32   ,L31   ,_NONE_       ,R31   ,R32   ,R30   ,_NONE_,_NONE_  \
+        L00   ,L01   ,L02   ,L03   ,L04   ,L05   ,R05   ,R04   ,R03   ,R02   ,R01   ,R00   , \
+        L10   ,L11   ,L12   ,L13   ,L14   ,L15   ,R15   ,R14   ,R13   ,R12   ,R11   ,R10   , \
+        L20   ,L21   ,L22   ,L23   ,L24   ,L25   ,R25   ,R24   ,R23   ,R22   ,R21   ,R20   , \
+        L26   ,L27   ,L31   ,L32   ,L34   ,L33          ,R34   ,R32   ,R31   ,R27   ,R26     \
     )
 // clang-format on
 
