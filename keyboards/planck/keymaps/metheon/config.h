e0c88fa186e797cc54607775cd0ebc5a4c51b94e
Kenneth Ehrenreich-Zak <kenneth@ehrenreich-zak.dk>
2021-03-26 08:17:38 +0100

    Swap thumb keys on Planck

--- keyboards/planck/keymaps/metheon/config.h
+++ keyboards/planck/keymaps/metheon/config.h
@@ -11,7 +11,7 @@
         L00   ,L01   ,L02   ,L03   ,L04   ,KC_NO ,KC_NO ,R04   ,R03   ,R02   ,R01   ,R00   , \
         L10   ,L11   ,L12   ,L13   ,L14   ,KC_NO ,KC_NO ,R14   ,R13   ,R12   ,R11   ,R10   , \
         L20   ,L21   ,L22   ,L23   ,L24   ,KC_NO ,KC_NO ,R24   ,R23   ,R22   ,R21   ,R20   , \
-        L30   ,KC_NO ,KC_NO ,L32   ,L31   ,KC_NO        ,R31   ,R32   ,KC_NO ,KC_NO ,R30     \
+        L30   ,KC_NO ,KC_NO ,L31   ,L32   ,KC_NO        ,R32   ,R31   ,KC_NO ,KC_NO ,R30     \
     )
 // clang-format on
 
