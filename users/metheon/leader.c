ce2ef73ed8e8b6dc9ef59a60fb910e22bddc4db9
Kenneth Ehrenreich-Zak <kenneth@ehrenreich-zak.dk>
2021-09-05 21:37:28 +0200

    Remove very secret secrets from keyboard

--- users/metheon/leader.c
+++ users/metheon/leader.c
@@ -41,87 +41,46 @@ void matrix_scan_leader(void) {
         leading = false;
         leader_end();
 
-        // /////////////////////// //
-        // Application Invocations //
-        // /////////////////////// //
-        SEQ_ONE_KEY(KC_P) { // Invoke Password Manager
-            SEND_STRING(SS_LCTL( SS_LALT( SS_LSFT("p") ) ) );
-        }
-
         // ////////////////////// //
         // Keyboard Related Stuff //
         // ////////////////////// //
         SEQ_TWO_KEYS(KC_Q, KC_M) {
-            SEND_STRING("qmk compile -c -kb" QMK_KEYBOARD " -km " QMK_KEYMAP SS_TAP(X_ENTER));
+            SEND_STRING("qmk compile -c -kb " QMK_KEYBOARD " -km " QMK_KEYMAP SS_TAP(X_ENTER));
         }
         SEQ_TWO_KEYS(KC_Q, KC_F) {
-            SEND_STRING("qmk flash -c -kb" QMK_KEYBOARD " -km " QMK_KEYMAP SS_TAP(X_ENTER));
+            SEND_STRING("qmk flash -c -kb " QMK_KEYBOARD " -km " QMK_KEYMAP SS_TAP(X_ENTER));
         }
         SEQ_TWO_KEYS(KC_Q, KC_V) {
             SEND_STRING(QMK_KEYBOARD "/" QMK_KEYMAP " @ " QMK_VERSION);
         }
 
-        // /////// //
-        // Secrets //
-        // /////// //
+        // ////////////////////// //
+        // Secrets (no passwords) //
+        // ////////////////////// //
 
-        // 0 not in use
-        SEQ_ONE_KEY(KC_P) {
+        SEQ_ONE_KEY(KC_N) {
+            // send my name
+            send_string(secrets[0]);
+        }
+        SEQ_TWO_KEYS(KC_S, KC_E) {
+            // encrypt the secrets file
             send_string(secrets[1]);
         }
-        SEQ_THREE_KEYS(KC_P, KC_N, KC_I) {
+        SEQ_TWO_KEYS(KC_S, KC_R) {
+            // rm the secrets file
             send_string(secrets[2]);
         }
-        SEQ_ONE_KEY(KC_N) {
+        SEQ_TWO_KEYS(KC_S, KC_D) {
+            // decrypt the secrets file
             send_string(secrets[3]);
         }
-        SEQ_TWO_KEYS(KC_P, KC_N) {
+        SEQ_TWO_KEYS(KC_P, KC_E) {
+            // send my private email
             send_string(secrets[4]);
         }
-        // 5 not in use
-        // 6 not in use
-        SEQ_TWO_KEYS(KC_S, KC_E) {
-            send_string(secrets[7]);
-        }
-        SEQ_TWO_KEYS(KC_S, KC_R) {
-            send_string(secrets[8]);
-        }
-        SEQ_TWO_KEYS(KC_S, KC_D) {
-            send_string(secrets[9]);
-        }
-        // 10 not in use
-        SEQ_TWO_KEYS(KC_P, KC_G) {
-            send_string(secrets[11]);
-        }
-        SEQ_TWO_KEYS(KC_P, KC_D) {
-            send_string(secrets[12]);
-        }
-        SEQ_TWO_KEYS(KC_P, KC_L) {
-            send_string(secrets[13]);
-        }
-        SEQ_TWO_KEYS(KC_P, KC_E) {
-            send_string(secrets[14]);
-        }
         SEQ_TWO_KEYS(KC_W, KC_E) {
-            send_string(secrets[15]);
-        }
-        SEQ_TWO_KEYS(KC_W, KC_L) {
-            send_string(secrets[16]);
-        }
-        SEQ_TWO_KEYS(KC_W, KC_G) {
-            send_string(secrets[17]);
-        }
-        SEQ_THREE_KEYS(KC_W, KC_S, KC_P) {
-            send_string(secrets[18]);
-        }
-        SEQ_THREE_KEYS(KC_W, KC_S, KC_T) {
-            send_string(secrets[19]);
-        }
-        SEQ_TWO_KEYS(KC_W, KC_O) {
-            send_string(secrets[20]);
-        }
-        SEQ_TWO_KEYS(KC_W, KC_M) {
-            send_string(secrets[21]);
+            // send my work email
+            send_string(secrets[5]);
         }
     }
 }
