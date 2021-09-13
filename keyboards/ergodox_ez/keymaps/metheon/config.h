98e42663f2a47a0ba4a00764970baac1985269d5
Kenneth Ehrenreich-Zak <kenneth@ehrenreich-zak.dk>
2021-09-13 20:48:54 +0200

    Add support for Ergodox EZ

---
+++ keyboards/ergodox_ez/keymaps/metheon/config.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// clang-format off
+#define LAYOUT_metheon( \
+        L00   ,L01   ,L02   ,L03   ,L04        ,R04   ,R03   ,R02   ,R01   ,R00   , \
+        L10   ,L11   ,L12   ,L13   ,L14        ,R14   ,R13   ,R12   ,R11   ,R10   , \
+        L20   ,L21   ,L22   ,L23   ,L24        ,R24   ,R23   ,R22   ,R21   ,R20   , \
+                      ENC01 ,L30   ,L31        ,R31   ,R30   ,ENC02                 \
+    ) \
+    LAYOUT_ergodox_pretty( \
+    KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO   ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO ,KC_NO , \
+    KC_NO ,KC_NO ,L01   ,L02   ,L03   ,L04   ,KC_NO   ,KC_NO ,R04   ,R03   ,R02   ,R01   ,KC_NO ,KC_NO , \
+    KC_NO ,L00   ,L11   ,L12   ,L13   ,L14                   ,R14   ,R13   ,R12   ,R11   ,R00   ,KC_NO , \
+    KC_NO ,L10   ,L21   ,L22   ,L23   ,L24   ,KC_NO   ,KC_NO ,R24   ,R23   ,R22   ,R21   ,R10   ,KC_NO , \
+    KC_NO ,L20   ,KC_NO ,KC_NO ,KC_NO                               ,KC_NO ,KC_NO ,KC_NO ,R20   ,KC_NO , \
+                                       KC_NO ,KC_NO   ,KC_NO ,KC_NO ,                                    \
+                                              KC_NO   ,KC_NO ,                                           \
+                                L30   ,L31   ,KC_NO   ,KC_NO ,R31   ,R30                                 \
+)
+// clang-format on
--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ cwd=$(pwd)
 ln -sf "$cwd/users/metheon/" "$cwd/../qmk_firmware/users/"
 ln -sf "$cwd/keyboards/splitkb/kyria/keymaps/metheon/" "$cwd/../qmk_firmware/keyboards/splitkb/kyria/keymaps/"
 ln -sf "$cwd/keyboards/planck/keymaps/metheon/" "$cwd/../qmk_firmware/keyboards/planck/keymaps/"
+ln -sf "$cwd/keyboards/ergodox_ez/keymaps/metheon/" "$cwd/../qmk_firmware/keyboards/ergodox_ez/keymaps/"
