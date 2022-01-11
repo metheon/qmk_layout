71d57ff587843b8d6ed57299fb1168fd683d3390
Kenneth Ehrenreich-Zak <kenneth@ehrenreich-zak.dk>
2022-01-11 14:10:08 +0100

    Add support for sweep and hands down titanium

--- users/metheon/metheon.c
+++ users/metheon/metheon.c
@@ -11,43 +11,66 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case HOME_A:
-        case HOME_R:
-        case HOME_S:
-        case HOME_T:
-        case HOME_N:
-        case HOME_E:
-        case HOME_I:
-        case HOME_O:
-             return true;
-        case TAB_SYM:
-        case SPC_NAV:
-        case BSPC_NUM:
-        case DEL_SYM:
-            return true;
-        default:
-            return false;
+          // colemak
+          case HOME_R:
+          case HOME_S:
+          case HOME_T:
+          case HOME_D:
+          case HOME_H:
+          case HOME_N:
+          case HOME_E:
+          case HOME_I:
+               return true;
+          // hands down
+          // (duplicated) case HDT_M_I:
+          // (duplicated) case HDT_M_E:
+          case HDT_M_A:
+          case HDT_M_U:
+          // (duplicated) case HDT_M_D:
+          // (duplicated) case HDT_M_T:
+          case HDT_M_N:
+          case HDT_M_S:
+          // thumb keys
+          case TAB_SYM:
+          case SPC_NAV:
+          case R_NUM:
+          case BSPC_SYM:
+               return true;
+          default:
+               return false;
     }
 }
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-        case HOME_A:
-        case HOME_R:
-        case HOME_S:
-        case HOME_T:
-        case HOME_N:
-        case HOME_E:
-        case HOME_I:
-        case HOME_O:
-            return TAPPING_TERM - 100; // 400-100=300
-        case TAB_SYM:
-        case SPC_NAV:
-        case BSPC_NUM:
-        case DEL_SYM:
-            return TAPPING_TERM - 225; // 400-225=175
-        default:
-            return TAPPING_TERM;       // 400
+          // colemak
+          case HOME_R:
+          case HOME_S:
+          case HOME_T:
+          case HOME_D:
+          case HOME_H:
+          case HOME_N:
+          case HOME_E:
+          case HOME_I:
+               return TAPPING_TERM - 100; // 400-100=300
+          // hands down
+          // (duplicated) case HDT_M_I:
+          // (duplicated) case HDT_M_E:
+          case HDT_M_A:
+          case HDT_M_U:
+          // (duplicated) case HDT_M_D:
+          // (duplicated) case HDT_M_T:
+          case HDT_M_N:
+          case HDT_M_S:
+               return TAPPING_TERM - 100; // 400-100=300
+          // thumb keys
+          case TAB_SYM:
+          case SPC_NAV:
+          case R_NUM:
+          case BSPC_SYM:
+               return TAPPING_TERM - 100; // 400-100=300
+          default:
+               return TAPPING_TERM;       // 400
     }
 }
 
@@ -63,29 +86,40 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //  .--------+--------+--------+--------+--------.   .--------+--------+--------+--------+--------.
          KC_AE   ,KC_W    ,KC_F    ,KC_P    ,KC_B    ,    KC_J    ,KC_L    ,KC_U    ,KC_Y    ,KC_QUOT ,
     //  |--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------|
-         HOME_A  ,HOME_R  ,HOME_S  ,HOME_T  ,KC_G    ,    KC_M    ,HOME_N  ,HOME_E  ,HOME_I  ,HOME_O  ,
+         KC_A    ,HOME_R  ,HOME_S  ,HOME_T  ,KC_G    ,    KC_M    ,HOME_N  ,HOME_E  ,HOME_I  ,KC_O    ,
     //  |--------+--------+--------+--------+--------|   |--------+--------|--------+--------+--------|
-         KC_AA   ,KC_X    ,KC_C    ,KC_D    ,KC_V    ,    KC_K    ,KC_H    ,KC_COMMA,KC_DOT  ,KC_OE   ,
+         KC_AA   ,KC_X    ,KC_C    ,HOME_D  ,KC_V    ,    KC_K    ,HOME_H  ,KC_COMMA,KC_DOT  ,KC_OE   ,
     //  '--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------'
-                           CHD_E01 ,TAB_SYM ,SPC_NAV ,    BSPC_NUM,DEL_SYM ,CHD_E02
+                           CHD_E01 ,TAB_SYM ,SPC_NAV ,    NUM     ,BSPC_SYM,CHD_E02
+    //                    '--------+--------+--------'   '--------+--------+--------'
+    ),
+    [_HDT] = LAYOUT_metheon(
+    //  .--------+--------+--------+--------+--------.   .--------+--------+--------+--------+--------.
+         KC_QUOT ,KC_DQT  ,KC_SLSH ,KC_DOT  ,KC_SCLN ,    KC_B    ,KC_P    ,KC_M    ,KC_G    ,KC_X    ,
+    //  |--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------|
+         KC_H    ,HDT_M_I ,HDT_M_E ,HDT_M_A ,KC_COMMA,    KC_W    ,HDT_M_T ,HDT_M_N ,HDT_M_S ,KC_C    ,
+    //  |--------+--------+--------+--------+--------|   |--------+--------|--------+--------+--------|
+         KC_K    ,KC_Y    ,KC_O    ,HDT_M_U ,KC_MINS ,    KC_V    ,HDT_M_D ,KC_L    ,KC_F    ,KC_J    ,
+    //  '--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------'
+                           CHD_E01 ,TAB_SYM ,SPC_NAV ,    R_NUM   ,BSPC_SYM,CHD_E02
     //                    '--------+--------+--------'   '--------+--------+--------'
     ),
     [_NAV] = LAYOUT_metheon(
     //  .--------+--------+--------+--------+--------.   .--------+--------+--------+--------+--------.
          PREV_TAB,NEXT_TAB,PRV_DSKT,NXT_DSKT,LOCK    ,    __NONE__,KC_BSPC ,KC_UP   ,KC_DEL  ,__NONE__,
     //  |--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------|
-         KC_LCTRL,KC_LALT ,KC_LSFT ,KC_LGUI ,SEARCH  ,    __NONE__,KC_LEFT ,KC_DOWN ,KC_RGHT ,KC_ENTER,
+         KC_LCTL ,KC_LALT ,KC_LSFT ,KC_LGUI ,SEARCH  ,    CAPSWORD,KC_LEFT ,KC_DOWN ,KC_RGHT ,__NONE__,
     //  |--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------|
-         UNDO    ,CUT     ,COPY    ,PASTE   ,SCRNSHT ,    __NONE__,CAPSWORD,SCRM_SNK,SNK_CASE,CPS_DSBL,
+         UNDO    ,CUT     ,COPY    ,PASTE   ,SCRNSHT ,    SCRM_SNK,KC_ENTER,__NONE__,__NONE__,__NONE__,
     //  |--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------|
-                           NAV_E01 ,________,________,    ________,________,NAV_E02
+                           NAV_E01 ,________,________,    ________,DEL_SYM ,NAV_E02
     //                    '--------+--------+--------'   '--------+--------+--------'
     ),
     [_NUM] = LAYOUT_metheon(
     //  .--------+--------+--------+--------+--------.   .--------+--------+--------+--------+--------.
          __NONE__,KC_7    ,KC_8    ,KC_9    ,__NONE__,    __NONE__,KC_EQL  ,KC_LPRN ,KC_RPRN ,__NONE__,
     //  |--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------|
-         __NONE__,KC_4    ,KC_5    ,KC_6    ,__NONE__,    __NONE__,KC_LGUI ,KC_LGUI ,KC_LALT ,KC_LCTL ,
+         __NONE__,KC_4    ,KC_5    ,KC_6    ,__NONE__,    __NONE__,KC_LGUI ,KC_LSFT ,KC_LALT ,KC_LCTL ,
     //  |--------+--------+--------+--------+--------|   |--------+--------|--------+--------+--------|
          __NONE__,KC_1    ,KC_2    ,KC_3    ,__NONE__,    __NONE__,KC_PLUS ,KC_MINS ,KC_ASTR ,KC_SLSH ,
     //  |--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------'
@@ -105,7 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_SYS] = LAYOUT_metheon(
     //  .--------+--------+--------+--------+--------.   .--------+--------+--------+--------+--------.
-         RESET   ,__NONE__,__NONE__,__NONE__,__NONE__,    __NONE__,M_L2_3RD,M_UP_HF ,M_R2_3RD,__NONE__,
+         RESET   ,__NONE__,__NONE__,TO_HDT  ,__NONE__,    __NONE__,M_L2_3RD,M_UP_HF ,M_R2_3RD,__NONE__,
     //  |--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------|
          __NONE__,KC_MPRV ,KC_MPLY ,KC_MNXT ,__NONE__,    M_FSCRN ,M_LT_HF ,M_BT_HF ,M_RT_HF ,__NONE__,
     //  |--------+--------+--------+--------+--------|   |--------+--------+--------+--------+--------|
--- users/metheon/metheon.h
+++ users/metheon/metheon.h
@@ -10,6 +10,7 @@
 
 enum layers {
     _COLDH,                 // Colemak-DH
+    _HDT,                   // Hands Down Titanium (neu-rx)
     _NAV,                   // Navigation cluster layer
     _NUM,                   // Numpad layer
     _SYM,                   // Symbols, both hands
@@ -19,28 +20,43 @@ enum layers {
 #define __NONE__            KC_NO
 #define ________            _______
 
- 
+
+// Colemak thumb keys 
 #define TAB_SYM             LT(_SYM, KC_TAB)
 #define SPC_NAV             LT(_NAV, KC_SPC)
-#define BSPC_NUM            LT(_NUM, KC_BSPC)
-#define DEL_SYM             LT(_SYM, KC_DEL)
+#define NUM                 MO(_NUM)
+#define BSPC_SYM            LT(_SYM, KC_BSPC)
 #define ZERO_NAV            LT(_NAV, KC_0)
+#define DEL_SYM             LT(_SYM, KC_DEL)
 
+// hands down thumb keys
+#define R_NUM               LT(_NUM, KC_R)
+
+#define TO_BASE             TO(_COLDH)
+#define TO_HDT              TO(_HDT)
 #define SHIFT               OSM(MOD_LSFT)
 
-#define LOWER               MO(_LOWER)
-#define RAISE               MO(_RAISE)
-#define SYMBOLS             OSL(_SYMBOLS)
-
-// Home row mods, using Colemak ... arst <--> neio
-#define HOME_A              LCTL_T(KC_A)
+// Home row mods, using Colemak ... rst/d <--> h/nei
 #define HOME_R              LALT_T(KC_R)
 #define HOME_S              LSFT_T(KC_S)
 #define HOME_T              LGUI_T(KC_T)
+#define HOME_D              LCTL_T(KC_D)
+#define HOME_H              LCTL_T(KC_H)
 #define HOME_N              LGUI_T(KC_N)
 #define HOME_E              LSFT_T(KC_E)
 #define HOME_I              LALT_T(KC_I)
-#define HOME_O              LCTL_T(KC_O)
+
+// Home row mods, using Hands Down Titanium ... iea/u <--> d/tns
+// Pinkies not used, instead ctrl is put under cmd
+
+#define HDT_M_I              LALT_T(KC_I)
+#define HDT_M_E              LSFT_T(KC_E)
+#define HDT_M_A              LGUI_T(KC_A)
+#define HDT_M_U              LCTL_T(KC_U)
+#define HDT_M_D              LCTL_T(KC_D)
+#define HDT_M_T              LGUI_T(KC_T)
+#define HDT_M_N              LSFT_T(KC_N)
+#define HDT_M_S              LALT_T(KC_S)
 
 // Home row mods, for the funpad and numpad... F5 F6 F7 F8 <--> 4 5 6 0
 #define HOME_F5             LCTL_T(KC_F5)
